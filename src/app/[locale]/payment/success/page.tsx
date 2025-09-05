fa927eaf7f825f67b84058b9a8228f18e7178b8d
KDF25 <karimov.damir.faridovich@gmail.com>
2025-09-05 22:11:20 +0500

    fix: fix sitemap index

--- src/app/[locale]/payment/success/page.tsx
+++ src/app/[locale]/payment/success/page.tsx
@@ -1,6 +1,15 @@
 import { FC } from "react";
 
 import { PaymentSuccessPage } from "@/page/student";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export const metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 const Page: FC = ({}) => {
 	return <PaymentSuccessPage />;
